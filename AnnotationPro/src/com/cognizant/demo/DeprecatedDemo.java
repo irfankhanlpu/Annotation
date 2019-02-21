3cea3705e6a82fb6b1bb492b952d43cd27fe9751
Irfan <35290514+irfankhanlpu@users.noreply.github.com>
2019-02-21 12:57:30 +0530

    Initial Version

---
+++ AnnotationPro/src/com/cognizant/demo/DeprecatedDemo.java
@@ -0,0 +1,23 @@
+package com.cognizant.demo;
+
+public class DeprecatedDemo {
+
+	@Deprecated
+	public void x()
+	{
+		System.out.println("Hello x");
+	}
+	
+	public void xNew()
+	{
+		System.out.println("Hello new x");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		java.util.Date date=new java.util.Date();
+		date.getMonth();
+
+	}
+
+}
---
+++ AnnotationPro/src/com/cognizant/demo/SuppressWarningDemo.java
@@ -0,0 +1,11 @@
+package com.cognizant.demo;
+
+public class SuppressWarningDemo {
+
+	@SuppressWarnings (value="unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int i;
+	}
+
+}
